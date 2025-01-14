21dcbd6c89978d1edfd7cb96cac4251e7fc4d567
MartaJover <martajover.design@gmail.com>
2025-01-14 07:14:59 -0500

    message

--- speedAnalysis/speed_analysis.js
+++ speedAnalysis/speed_analysis.js
@@ -28,7 +28,9 @@ function endTest() {
     // Split the text using regex to count words correctly
     var typedWords = userTypedText.split(/\s+/).filter(function (word) {
         return word !== "";
-    }).length;
+    }).length;    
+
+    var totalLength = userTypedText.length;
 
     var wpm = 0; // Default value
 
@@ -39,6 +41,7 @@ function endTest() {
     // Display the results
     var outputDiv = document.getElementById("output");
     outputDiv.innerHTML = "<h2>Typing Test Results:</h2>" +
+        "<p>Total Length: " + totalLength + "</p>" +
         "<p>Words Typed: " + typedWords + "</p>" +
         "<p>Time Elapsed: " + timeElapsed.toFixed(2) + " seconds</p>" +
         "<p>Words Per Minute (WPM): " + wpm + "</p>";
